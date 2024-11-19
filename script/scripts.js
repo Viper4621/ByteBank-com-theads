53fbe72e21625bd8b3fa50c7f5cf91bb12c7bdca
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2024-11-19 19:32:29 -0300

    Add files via upload
    Implementando WebWorks e aprendendo a respeito sua função em relação a gerar mult theads que evitam travamentos / aliviam os processos / assim evitados erros futuros, travamentos e loops de erros

--- script/scripts.js
+++ script/scripts.js
@@ -4,47 +4,40 @@ const graficoDolar = document.getElementById('graficoDolar');
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-      labels: [],
-      datasets: [{
-        label: 'Dólar',
-        data: [],
-        borderWidth: 1
-      }]
+        labels: [],
+        datasets: [{
+            label: 'Dólar',
+            data: [],
+            borderWidth: 1
+        }]
     },
-  });
-//criamos uma função assincrona para esperar a busca da api de cotação e abaixo criamos o armazenamento para transformar
-//atraves do json em dados para utilizarmos
-setInterval(()=> conectaApi(), 5000);
-  async function conectaApi(){
+});
+setInterval(() => conectaAPI(), 5000);
+async function conectaAPI() {
     const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
     const conectaTraduzido = await conecta.json();
-    //aqui criamos uma let pois tem alteração de data para acessar a função de pegar hora minutos e segundos
-    let tempo = gerarHorario();
-    //aqui de acordo com a api de cotação para buscar dentro do objeto USDBRL o valor ask que é o preço da cotação
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo , valor)
-    //depois de pronta a função imprime cotação agora colocamos na nossa função 
-    imprimeCotacao("dolar", valor);
-  }
-//aqui definimos um intervalo de requisição para atualizar os dados da função acima
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask
+    adicionarDados(graficoParaDolar, tempo, valor);
+    imprimeCotacao("dolar", valor)
+}
 
-
-
-//criamos um função abaixo para pegar horario horas minutos e segundos
-function gerarHorario(){
+function geraHorario() {
     let data = new Date();
     let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
-    console.log(horario)
+    console.log(horario);
     return horario;
 }
-//função com 3 parametros
-//grafico que vamos criar / data / labels para acessar os dados do chart.js e pegar labels push é para inserir na lista
-function adicionarDados(grafico, legenda, dados){
+
+function adicionarDados(grafico, legenda, dados) {
     grafico.data.labels.push(legenda);
-    //abaixo é igual acima no caso da estrutura de dados do data chart.js para acessar o data e push para inserir na lista
-    grafico.data.datasets.forEach((dataset) =>{
+    grafico.data.datasets.forEach((dataset) => {
         dataset.data.push(dados);
     })
-    //documentação diz para sempre no final usar abaixo para receber e atualizar o grafico
     grafico.update();
 }
+//workers serve para executar theads em 2 plano usamos para criar novos trabalhadores para executar partes do processo em 2 plano 
+//aliviando peso
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+//estamos selecionando o worker e mandando uma mensagem de usd pra especificar/ mult thead / isso evita travar navegador
+workerDolar.postMessage('usd');
